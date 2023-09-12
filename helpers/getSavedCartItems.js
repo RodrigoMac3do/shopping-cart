7ffa08436aa34abc4232f267271df65401bae495
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-09-12 14:01:10 -0300

    docs: documenta funções

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,4 +1,14 @@
-const getSavedCartItems = () => localStorage.getItem('cartItems');
+/**
+ * Função para recuperar itens do carrinho salvos no Local Storage.
+ *
+ * @returns {Array} Um array de itens do carrinho recuperados do Local Storage.
+ *
+ * @example
+ * const savedItems = getSavedCartItems();
+ * console.log(savedItems); // Array de itens do carrinho recuperados.
+ */
+const getSavedCartItems = () =>
+  JSON.parse(localStorage.getItem('cartItems')) || [];
 
 if (typeof module !== 'undefined') {
   module.exports = getSavedCartItems;
--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,4 +1,18 @@
-const saveCartItems = (name) => localStorage.setItem('cartItems', name);
+/**
+ * Função para salvar itens do carrinho no Local Storage.
+ *
+ * @param {Array} produtos - Um array de objetos representando os itens do carrinho a serem salvos.
+ *
+ * @example
+ * const cartItems = [
+ *   { id: 'MLB12345', title: 'Produto 1', price: 19.99 },
+ *   { id: 'MLB67890', title: 'Produto 2', price: 29.99 }
+ * ];
+ * saveCartItems(cartItems);
+ * // Os itens do carrinho são salvos no Local Storage.
+ */
+const saveCartItems = (produtos) =>
+  localStorage.setItem('cartItems', JSON.stringify(produtos));
 
 if (typeof module !== 'undefined') {
   module.exports = saveCartItems;
